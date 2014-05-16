48e262726e24a30bd29d2fd0a52f7b3afa8e4f06
Johan Attali <johan.attali@gmail.com>
2014-05-16 16:39:29 +0200

    [services] added XPSourceFetcher raw class

---
+++ xpod/services/XPSourceFetcher.h
@@ -0,0 +1,13 @@
+//
+//  XPSourceFetcher.h
+//  xpod
+//
+//  Created by Johan on 16/05/14.
+//  Copyright (c) 2014 dispatchasync. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XPSourceFetcher : NSObject
+
+@end
---
+++ xpod/services/XPSourceFetcher.m
@@ -0,0 +1,13 @@
+//
+//  XPSourceFetcher.m
+//  xpod
+//
+//  Created by Johan on 16/05/14.
+//  Copyright (c) 2014 dispatchasync. All rights reserved.
+//
+
+#import "XPSourceFetcher.h"
+
+@implementation XPSourceFetcher
+
+@end
