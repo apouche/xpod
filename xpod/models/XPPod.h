7147e27ed892a2fb017826df2b3c7f2f6c7d2451
Johan Attali <johan.attali@gmail.com>
2014-05-16 16:39:07 +0200

    [model] added XPPod model subclass

---
+++ xpod/models/XPPod.h
@@ -0,0 +1,16 @@
+//
+//  XPPod.h
+//  xpod
+//
+//  Created by Johan on 16/05/14.
+//  Copyright (c) 2014 dispatchasync. All rights reserved.
+//
+
+#import "XPObject.h"
+
+@interface XPPod : XPObject
+@property (nonatomic, strong) NSString* name;
+@property (nonatomic, strong) NSURL* url;
+@property (nonatomic, strong) NSString* sourcePath;
+
+@end
---
+++ xpod/models/XPPod.m
@@ -0,0 +1,13 @@
+//
+//  XPPod.m
+//  xpod
+//
+//  Created by Johan on 16/05/14.
+//  Copyright (c) 2014 dispatchasync. All rights reserved.
+//
+
+#import "XPPod.h"
+
+@implementation XPPod
+
+@end
