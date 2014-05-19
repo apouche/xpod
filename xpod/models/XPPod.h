940d778da84b44cde1dbaacde598d6a388f0dc81
Johan Attali <johan.attali@gmail.com>
2014-05-19 15:47:57 +0200

    [model] added url and sourcePattern properties to XPPod

--- xpod/models/XPPod.h
+++ xpod/models/XPPod.h
@@ -8,10 +8,13 @@
 
 #import "XPObject.h"
 
+extern const NSString* kXPPodURLKey;
+extern const NSString* kXPPodSourceKey;
+
 @interface XPPod : XPObject
 @property (nonatomic, strong) NSString* name;
 @property (nonatomic, strong) NSURL* url;
-@property (nonatomic, strong) NSString* sourcePath;
+@property (nonatomic, strong) NSString* sourcePattern;
 
 - (id)initWithYAMLDictionary:(NSDictionary*)dictionary name:(NSString*)name;
 
--- xpod/models/XPPod.m
+++ xpod/models/XPPod.m
@@ -8,11 +8,16 @@
 
 #import "XPPod.h"
 
+const NSString* kXPPodSourceKey = @"src";
+const NSString* kXPPodURLKey = @"git";
+
 @implementation XPPod
 
 - (id)initWithYAMLDictionary:(NSDictionary *)dictionary name:(NSString*)name {
     if (self = [super init]) {
         self.name = name;
+        self.url = [NSURL URLWithString:dictionary[kXPPodURLKey]];
+        self.sourcePattern = dictionary[kXPPodSourceKey];
     }
     return self;
 }
